a412bbe68e2bce049e2d70047aa32e753a6df337
ctllo-bit <johndoe@example.com>
2026-05-12 21:43:44 +0800

    o

--- app/ui/gateway.js
+++ app/ui/gateway.js
@@ -3,31 +3,31 @@ const httpProxy = require("http-proxy");
 const fs = require("fs");//删除旧 socket 文件。
 
 const GATEWAY_SOCKET ="/var/apps/coder/target/gateway.sock";
-const CODE_SOCKET ="/var/apps/coder/target/code-server.sock";
+const CODESERVER_SOCKET ="/var/apps/coder/target/code-server.sock";
 
-try {//删除旧 socket
-  fs.unlinkSync(GATEWAY_SOCKET);
-} catch {}
+// 如果旧socket存在则删除
+if (fs.existsSync(GATEWAY_SOCKET)) {
+    fs.unlinkSync(GATEWAY_SOCKET);
+}
 
-//创建代理对象
+//创建代理对象，把收到的请求转发给 code-server.sock
 const proxy = httpProxy.createProxyServer({
   target: {
-    socketPath: CODE_SOCKET
+    socketPath: CODESERVER_SOCKET
   },
   ws: true
 });
 
-
-//普通 HTTP 请求处理器,创建 HTTP 服务器。
+//创建HTTP Server
 const server = http.createServer((req, res) => {
-
-  req.url = req.url.replace(/^\/app\/coder(?=\/|$)/, '') || '/';
-
-  proxy.web(req, res);//转发 HTTP 请求
+    //gateway.sock接收 HTTP 请求,并处理处理 req
+    req.url = req.url.replace(/^\/app\/coder(?=\/|$)/, '') || '/';
+    proxy.web(req, res);//转发 HTTP 到code-server.sock
 });
 
-
-
+// 监听文件路径而非端口
 server.listen(GATEWAY_SOCKET, () => {
-  console.log("gateway listening:", GATEWAY_SOCKET);
+    // 赋予权限
+    fs.chmodSync(GATEWAY_SOCKET, '0666');
+    console.log(`Server listening on ${GATEWAY_SOCKET}`);
 });
